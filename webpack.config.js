7677f57cf9f32f810a98466abd0c950973e5da26
Jon Sakas <jon.sakas@gmail.com>
2018-10-05 21:48:20 -0600

    Create dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Jon Sakas',
+    })
+  ]
 };
