8113c31a892a20c4b8aad8d4274885932c51594f
Jon Sakas <jon.sakas@gmail.com>
2018-10-07 10:35:28 -0600

    Write Typer component to dynamically type strings onto page

---
+++ src/Typer.js
@@ -0,0 +1,64 @@
+import React, { Component, Fragment } from 'react';
+
+export default class Typer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      target: props.text,
+      text: props.text.split('').map(i => {
+        return {
+          target: i,
+          count: 0,
+          max: Math.floor(Math.random() * 50),
+          current: String.fromCharCode(Math.floor(Math.random() * 25) + 97)
+        }
+      })
+    };
+  }
+
+  componentDidMount() {
+    const refresh = setInterval(() => {
+      if (this.state.text.map(i => i.current).join('') == this.props.text) {
+        clearInterval(refresh);
+        this.props.onComplete();
+      }
+      this.iterate();
+    }, 10);
+  }
+
+  iterate = () => {
+    this.setState(prevState => {
+      return {
+        text: prevState.text.map(i => {
+          if (i.current == i.target) {
+            return i;
+          }
+
+          if (i.count >= i.max) {
+            i.current = i.target;
+            return i;
+          }
+
+          i.count += 1;
+          i.current = String.fromCharCode(Math.floor(Math.random() * 25) + 97);
+
+          return i;
+        }),
+      }
+    })
+  }
+
+  render() {
+    return this.state.text.map((i, index) => {
+      return (
+        <Fragment key={index}>{i.current}</Fragment>
+      );
+    });
+  }
+}
+
+Typer.defaultProps = {
+  text: '',
+  onComplete: () => {},
+};
