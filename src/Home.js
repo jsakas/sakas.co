f084b8aaa05e200c59e4ad9d8a4b7d876b658e89
Jon Sakas <jon.sakas@gmail.com>
2018-10-07 10:45:53 -0600

    Add Typer to Home page

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import Galaxy from './Galaxy';
+import Typer from './Typer';
 
 import './Home.scss';
 
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      phase1: false,
+      phase2: false,
+      phase3: false,
+    };
+  }
+  onTyperComplete = (phase) => () => {
+    this.setState({
+      [phase]: true,
+    })
+  }
+
   render() {
     return (
       <div className="Home">
@@ -12,9 +28,21 @@ export default class Home extends Component {
           <Galaxy />
         </div>
         <div className="Home__intro">
-          <h1>Greetings</h1>
-          <p>You have reached the home page of Jon Sakas.</p>
-          <p>A human from Denver, CO, USA, Earth.</p>
+          <h1>
+            <Typer text="Greetings" onComplete={this.onTyperComplete('phase1')} />
+          </h1>
+          
+          {this.state.phase1 && (
+            <p>
+              <Typer text="You have reached the home page of Jon Sakas" onComplete={this.onTyperComplete('phase2')} />
+            </p>
+          )}
+
+          {this.state.phase2 && (
+            <p>
+              <Typer text="A human from Denver, Colorado, USA, Earth, Milky Way." onComplete={this.onTyperComplete('phase3')} />
+            </p>
+          )}
         </div>
       </div>
     );
