822be7d4a9643ef26300e709756c9e07de759388
Jon Sakas <jon.sakas@gmail.com>
2018-10-28 01:11:58 -0600

    Add a couple placeholder Experiments

--- src/pages/experiments/Experiments.js
+++ src/pages/experiments/Experiments.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './Experiments.scss';
 
-const Experiments = () => {
-  return (
-    <div className="Experiments page page--padded">
-      
-    </div>
-  );
-};
+const EXPERIMENTS = [
+  {
+    title: 'Subtractor',
+  },
+  {
+    title: 'Prototype',
+  },
+  {
+    title: 'AudioJS',
+  }
+];
+
+class Experiment extends Component {
+  render() {
+    return (
+      <div className="Experiment">
+        {this.props.title}
+      </div>
+    );
+  }
+}
+
+class Experiments extends Component {
+  render() {
+    return (
+      <div className="Experiments page page--padded">
+        <div className="Experiments__grid">
+          {
+            EXPERIMENTS.map((props, i) => {
+              return (
+                <Experiment key={i} {...props} />
+              );
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Experiments;
