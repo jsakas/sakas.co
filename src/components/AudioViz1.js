d592ae691d8a80ee5bef4b185d487be0a80943bf
Jon Sakas <jon.sakas@gmail.com>
2018-11-02 22:30:59 -0600

    Write simple audio interface and load loop

--- src/components/AudioViz1.js
+++ src/components/AudioViz1.js
@@ -2,8 +2,46 @@
  * A React component template for drawing on the canvas with requestAnimationFrame.
  */
 import React, { Component } from 'react';
+import audioLoop from '@audio/looper.wav';
 
 const ID = 'AudioViz1';
+
+class Audio {
+  constructor() {
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    this._context = new AudioContext();
+
+  }
+
+  decodeAudioDataPromise = (buffer) => {
+    return new Promise((resolve, reject) => {
+      this._context.decodeAudioData(buffer, resolve, reject);
+    });
+  }
+
+  getSourceFromUrl = (url) => {
+    return fetch(url)
+      .then(response => response.arrayBuffer())
+      .then(this.decodeAudioDataPromise)
+      .then(data => {
+        const source = this._context.createBufferSource();
+        source.buffer = data;
+        return source;
+      });
+  }
+
+  get destination() {
+    return this._context.destination;
+  }
+}
+
+const audio = new Audio();
+
+audio.getSourceFromUrl(audioLoop).then(source => {
+  source.loop = true;
+  source.connect(audio.destination);
+  source.start(0);
+});
 
 export default class CanvasDrawing extends Component {
   componentDidMount(){
@@ -51,6 +89,10 @@ const startGalaxy = () => {
   const draw = () => {
     context.clearRect(0, 0, WINW, WINH);
 
+    context.arc(WINW / 2, WINH / 2, 10, 1, Math.PI * 2);
+    context.fillStyle='blue';
+    context.fill();
+    
   };
 
   const update = () => {
