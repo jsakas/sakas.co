95d4bdf42ed6c45a996dd8b1b0d1ac8e81471654
Jon Sakas <jon.sakas@gmail.com>
2018-11-02 22:01:32 -0600

    Create high level routing for / scratch with skips the main App

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Pulse from '@components/pulse/Pulse';
 import Triangles from '@components/Triangles';
 import ROUTES from '@routes';
 
+import AudioViz1 from '@components/AudioViz1';
+
 import './App.scss';
 
 const getBaseRoute = location => location.pathname.split('/').filter(i => i)[0];
@@ -27,87 +29,94 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="App__canvas">
-          <Triangles />
-        </div>
-        <Router history={history}>
-          <Route key={getBaseRoute(history.location)} render={({ location }) => {
+      <Router history={history}>
+        <Switch>
+          <Route key={history.location.pathname} path="/scratch/:id?" render={() => {
             return (
-              <TransitionGroup component={null}>
-
-                <Transition key={'logo'} timeout={1000}>
-                  {(state) => {
-                    return (
-                      <div className={`App__logo App__logo-transition App__logo-transition--${state}`}>
-                      jon.sakas
-                      </div>
-                    );
-                  }}
-                </Transition>
-
-
-                <Transition key={`${getBaseRoute(history.location)}--title`} timeout={3000}>
-                  {(state) => {
-                    return (
-                      <div className={'App__title'}>
-                        <Switch location={location}>
-                          {Object.keys(ROUTES).filter(route => ROUTES[route].showTitle).map((route) => {
-                            let title = ROUTES[route].title;
-                            return (
-                              <Route 
-                                exact
-                                key={location}
-                                path={ROUTES[route].path} 
-                                render={() => <h1 className={`App__title-transition App__title-transition--${state}`}>{title}</h1>}
-                              />
-                            );
-                          })}
-                        </Switch>
-                      </div>
-                    );
-                  }}
-                </Transition>
-
-                <Transition key={`${history.location.pathname}--page`} timeout={1000}>
-                  {(state) => {
-                    return (
-                      <div className={`App__page App__page-transition App__page-transition--${state}`}>
-                        <Switch location={location}>
-                          {Object.keys(ROUTES).map((route) => {
-                            let Page = ROUTES[route].component;
-                            return (
-                              <Route 
-                                exact
-                                key={location}
-                                path={ROUTES[route].path} 
-                                render={() => {
-                                  return (
-                                    <div className="App__page-wrapper">
-                                      <div className="App__page-content">
-                                        <Page />
-                                      </div>
-                                    </div>
-                                  );
-                                }}
-                              />
-                            );
-                          })}
-                        </Switch>
-                      </div>
-                    );
-                  }}
-                </Transition>
-
-              </TransitionGroup>
+              <AudioViz1 />
             );
           }} />
-        </Router>
-        <div className="App__menu-toggle" style={{ display: 'none' }}>
-          <Pulse onClick={this.toggleMenu} />
-        </div>
-        <Menu open={this.state.menuOpen} />
-      </div>
+          <div className="App">
+            <div className="App__canvas">
+              <Triangles />
+            </div>
+            <Route key={getBaseRoute(history.location)} render={({ location }) => {
+              return (
+                <TransitionGroup component={null}>
+
+                  <Transition key={'logo'} timeout={1000}>
+                    {(state) => {
+                      return (
+                        <div className={`App__logo App__logo-transition App__logo-transition--${state}`}>
+                      jon.sakas
+                        </div>
+                      );
+                    }}
+                  </Transition>
+
+
+                  <Transition key={`${getBaseRoute(history.location)}--title`} timeout={3000}>
+                    {(state) => {
+                      return (
+                        <div className={'App__title'}>
+                          <Switch location={location}>
+                            {Object.keys(ROUTES).filter(route => ROUTES[route].showTitle).map((route) => {
+                              let title = ROUTES[route].title;
+                              return (
+                                <Route 
+                                  exact
+                                  key={location}
+                                  path={ROUTES[route].path} 
+                                  render={() => <h1 className={`App__title-transition App__title-transition--${state}`}>{title}</h1>}
+                                />
+                              );
+                            })}
+                          </Switch>
+                        </div>
+                      );
+                    }}
+                  </Transition>
+
+                  <Transition key={`${history.location.pathname}--page`} timeout={1000}>
+                    {(state) => {
+                      return (
+                        <div className={`App__page App__page-transition App__page-transition--${state}`}>
+                          <Switch location={location}>
+                            {Object.keys(ROUTES).map((route) => {
+                              let Page = ROUTES[route].component;
+                              return (
+                                <Route 
+                                  exact
+                                  key={location}
+                                  path={ROUTES[route].path} 
+                                  render={() => {
+                                    return (
+                                      <div className="App__page-wrapper">
+                                        <div className="App__page-content">
+                                          <Page />
+                                        </div>
+                                      </div>
+                                    );
+                                  }}
+                                />
+                              );
+                            })}
+                          </Switch>
+                        </div>
+                      );
+                    }}
+                  </Transition>
+
+                </TransitionGroup>
+              );
+            }} />
+            <div className="App__menu-toggle" style={{ display: 'none' }}>
+              <Pulse onClick={this.toggleMenu} />
+            </div>
+            <Menu open={this.state.menuOpen} />
+          </div>
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/components/AudioViz1.js
@@ -0,0 +1,62 @@
+/**
+ * A React component template for drawing on the canvas with requestAnimationFrame.
+ */
+import React, { Component } from 'react';
+
+const ID = 'AudioViz1';
+
+export default class CanvasDrawing extends Component {
+  componentDidMount(){
+    this.raf = startGalaxy();
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  componentWillUnmount() {
+    cancelAnimationFrame(this.raf);
+  }
+
+  render() {
+    return (<canvas className={ID} id={ID}></canvas>);
+  }
+}
+
+const r = Math.random;
+
+const startGalaxy = () => {
+  const canvas = document.getElementById(ID);
+  const context = canvas.getContext('2d');
+
+  let WINW, WINH;
+
+  const updateCanvasSize = () => {
+    WINW = window.innerWidth;
+    WINH = window.innerHeight;
+
+    canvas.setAttribute('width', WINW);
+    canvas.setAttribute('height', WINH);
+  };
+
+  window.addEventListener('resize', updateCanvasSize);
+  updateCanvasSize();
+
+  const particlesFactory = () => {
+    const particle = {};
+
+    return particle;
+  };
+
+  const draw = () => {
+    context.clearRect(0, 0, WINW, WINH);
+
+  };
+
+  const update = () => {
+    draw();
+    return requestAnimationFrame(update);
+  };
+
+  return update();
+};
