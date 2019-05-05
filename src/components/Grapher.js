13f7bed697a96897eb08da435fcf7f5bd3783264
Jon Sakas <jon.sakas@gmail.com>
2019-05-05 01:20:13 -0600

    Allow multiple functions to be drawn at once

--- src/components/Grapher.js
+++ src/components/Grapher.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import Canvas from '@components/canvas/Canvas';
 
-const r = Math.random;
+import './Grapher.scss';
 
 const COLOR_PRIMARY = 'rgb(203, 54, 98, .5)';
 const COLOR_SECONDARY = 'rgb(143, 82, 128, .7)';
 
-const draw = (canvas, context, fn) => {
+const drawAxis = (canvas, context) => {
   let w = canvas.width;
   let h = canvas.height;
   
   // translate & draw x / y axis
   context.translate(w / 2, h / 2);
-  
+
   context.beginPath();
   context.strokeStyle = COLOR_PRIMARY;
   context.moveTo(-w / 2, 0);
@@ -25,8 +25,18 @@ const draw = (canvas, context, fn) => {
   context.lineTo(0, h / 2);
   context.stroke();
   context.closePath();
+  
+  // reset translate before next draw
+  context.translate(-w / 2, -h / 2);
+};
 
-
+const drawFunction = (canvas, context, fn) => {
+  let w = canvas.width;
+  let h = canvas.height;
+  
+  // translate & draw x / y axis
+  context.translate(w / 2, h / 2);
+  
   context.strokeStyle = COLOR_SECONDARY;
   
   context.beginPath();
@@ -52,16 +62,44 @@ const draw = (canvas, context, fn) => {
 };
 
 
-const Visual = () => {
-  const fn = Math.cos;
+class Visual extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <Canvas
-      draw={(canvas, context) => draw(canvas, context, fn)}
-      id="Galaxy"
-      className="galaxy"
-    />
-  );
-};
+    this.state = {
+      fns: [
+        Math.cos,
+        Math.sin,
+        Math.log,
+      ]
+    };
+  }
+
+  render() {
+    let { fns } = this.state;
+
+    return (
+      <Fragment>
+        <Canvas
+          draw={(canvas, context) => {
+            drawAxis(canvas, context);
+            fns.forEach(fn => drawFunction(canvas, context, fn));
+          }}
+          id="grapher"
+        />
+        <div className="grapher-info">
+          {fns.map((fn, i) => {
+            return (
+              <div key={i}>
+                {String(fn)}
+              </div>
+            );
+          })}
+        </div>
+      </Fragment>
+    );
+  }
+}
+
 
 export default Visual;
